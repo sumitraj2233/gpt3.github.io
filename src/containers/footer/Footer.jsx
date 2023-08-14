8726dd8092c06ae7c4fda73710cee39b498048e7
sumitraj2233 <vinodkumar983541@gmail.com>
2023-08-14 15:12:59 +0530

    sss

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
         </div>
       </div>
       <div className='gpt3__footer-copyrights'>
-        <p>© 2023 GPT-SUMIT. All rights reserved.</p>
+        <p>© 2023 GPT-IRIMIT. All rights reserved.</p>
       </div>
     </div>
   )
